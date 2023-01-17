5d5deed6da8c9d3b462627f8f9f0320be3559837
karol <karolina.majkowska@o2.pl>
2023-01-17 23:27:28 +0100

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
 import java.awt.*;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
         Scene s1 = new Scene(600,600);
-        //Graphics g1 = s1.getGraphics();
        Circle c = new Circle(100, new Point(100, 100), true);
        RenderBoundingBox circle = new RenderBoundingBox(c);
-        Triangle t1 = new Triangle(new Point(400,200), new Point(500,200), new Point(450,300),true);
-       RenderBoundingBox triangle = new RenderBoundingBox(t1);
-       s1.addItem(t1);
-       triangle.drawBoundingBox();
+       Triangle t = new Triangle(new Point(400,200), new Point(500,200), new Point(450,300),true);
+       RenderBoundingBox triangle = new RenderBoundingBox(t);
+       Segment s = new Segment(new Point(500, 300), new Point(200, 400));
+       RenderBoundingBox segment = new RenderBoundingBox((s));
+       s1.addItem(circle);
+       s1.addItem(triangle);
+       s1.addItem(segment);
+
+       for(int i=0; i<s1.getItems().size(); i++)
+       {
+           System.out.println(i+1);
+       }
+
+       int answer;
+       answer = scan.nextInt();
+       s1.getItems().get(answer-1).drawBoundingBox();
+
+
     }
 
 }
